7c53104654eeeca26392444c2b5f08c6d3881892
Arvin-Chiang <arvinchiang673@gmail.com>
2025-07-21 15:34:32 +0800

    Add files via upload

---
+++ APCS_Question009/APCS_Question009/APCS_Question009.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main() {
+    string X;
+    cin >> X;
+
+    int A = 0, B = 0;
+
+    for (size_t i = 0; i < X.length(); ++i) {
+        int digit = X[i] - '0';
+
+        if ((i + 1) % 2 == 1) {
+            A += digit; // 奇數位（1-based）
+        }
+        else {
+            B += digit; // 偶數位
+        }
+    }
+
+    cout << abs(A - B) << endl;
+
+    return 0;
+}
