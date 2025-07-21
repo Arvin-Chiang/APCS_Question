637065295187f403cc6dcd9da7507e9f037c8dd3
Arvin-Chiang <arvinchiang673@gmail.com>
2025-07-21 16:39:54 +0800

    Add files via upload

---
+++ APCS_Question013/APCS_Question013/APCS_Question013.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <vector>
+using namespace std;
+
+struct Point {
+    double x, y;
+};
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<Point> polygon(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> polygon[i].x >> polygon[i].y;
+    }
+
+    double area = 0.0;
+    for (int i = 0; i < n; ++i) {
+        int j = (i + 1) % n;  // 下一个點（環狀）
+        area += (polygon[i].x * polygon[j].y) - (polygon[j].x * polygon[i].y);
+    }
+
+    area = abs(area) / 2.0;
+
+    cout << fixed << setprecision(2) << area << endl;
+
+    return 0;
+}
