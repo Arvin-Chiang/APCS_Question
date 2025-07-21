a6a42bc3675bee73623a343a09c7eea99267adda
Arvin-Chiang <arvinchiang673@gmail.com>
2025-07-21 15:39:59 +0800

    Add files via upload

---
+++ APCS_Question010/APCS_Question010/APCS_Question010.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+char rot13(char c) {
+    if (c >= 'a' && c <= 'z') {
+        return 'a' + (c - 'a' + 13) % 26;
+    }
+    else if (c >= 'A' && c <= 'Z') {
+        return 'A' + (c - 'A' + 13) % 26;
+    }
+    return c; // 非字母原樣輸出
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        for (char c : line) {
+            cout << rot13(c);
+        }
+        cout << endl;
+    }
+    return 0;
+}
