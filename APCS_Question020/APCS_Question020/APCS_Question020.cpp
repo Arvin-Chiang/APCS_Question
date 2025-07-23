bdf8f5e78c15d552bfe5be0a7fb12dcc6351363f
Arvin-Chiang <arvinchiang673@gmail.com>
2025-07-23 16:30:22 +0800

    Add files via upload

---
+++ APCS_Question020/APCS_Question020/APCS_Question020.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+int main() {
+    int N;
+    while (cin >> N && N != 0) {
+        while (true) {
+            vector<int> target(N);
+            cin >> target[0];
+            if (target[0] == 0) {
+                cout << endl;
+                break;
+            }
+
+            for (int i = 1; i < N; ++i) cin >> target[i];
+
+            stack<int> station;
+            int current = 1; 
+            int index = 0;  
+
+            while (current <= N) {
+                station.push(current);
+
+                while (!station.empty() && station.top() == target[index]) {
+                    station.pop();
+                    index++;
+                }
+
+                current++;
+            }
+
+            if (station.empty()) cout << "Yes\n";
+            else cout << "No\n";
+        }
+    }
+    return 0;
+}
