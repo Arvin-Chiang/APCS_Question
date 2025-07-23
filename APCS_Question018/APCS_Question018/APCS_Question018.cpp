803c98c7e1aea22dd8cf6de5799de4c4b27795e5
Arvin-Chiang <arvinchiang673@gmail.com>
2025-07-23 15:56:38 +0800

    Add files via upload

---
+++ APCS_Question018/APCS_Question018/APCS_Question018.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+
+struct Document {
+    int index;      
+    int priority;   
+};
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int n, m;
+        cin >> n >> m;
+        queue<Document> q;
+        int count[10] = {}; 
+
+        for (int i = 0; i < n; ++i) {
+            int p;
+            cin >> p;
+            q.push({ i, p });
+            count[p]++;
+        }
+
+        int time = 0;
+        while (!q.empty()) {
+            Document cur = q.front();
+            q.pop();
+
+         
+            bool hasHigher = false;
+            for (int i = cur.priority + 1; i <= 9; ++i) {
+                if (count[i] > 0) {
+                    hasHigher = true;
+                    break;
+                }
+            }
+
+            if (hasHigher) {
+              
+                q.push(cur);
+            }
+            else {
+               
+                time++;
+                count[cur.priority]--;
+                if (cur.index == m) {
+                    cout << time << endl;
+                    break;
+                }
+            }
+        }
+    }
+
+    return 0;
+}
