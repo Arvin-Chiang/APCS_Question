b0ea35e73d31b6727de4aca02cc1bc3c34b387b6
Arvin-Chiang <arvinchiang673@gmail.com>
2025-07-23 15:19:42 +0800

    Add files via upload

---
+++ APCS_Question017/APCS_Question017/APCS_Question017.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+int countValidBrackets(const string& expr) {
+    stack<char> s;
+    int pairCount = 0;
+
+    for (char c : expr) {
+        if (c == '(') {
+            s.push(c);
+        }
+        else if (c == ')') {
+            if (!s.empty()) {
+                s.pop();
+                pairCount++;
+            }
+            else {
+               
+                return 0;
+            }
+        }
+        else {
+         
+            return 0;
+        }
+    }
+
+    
+    if (!s.empty()) return 0;
+
+    return pairCount;
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        if (line.empty()) continue;  
+        cout << countValidBrackets(line) << endl;
+    }
+    return 0;
+}
