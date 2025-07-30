23ed38aee74971235a920b7cb27e836612c8c3b6
Arvin-Chiang <arvinchiang673@gmail.com>
2025-07-30 16:28:22 +0800

    Add files via upload

---
+++ APCS_Question032/APCS_Question032/APCS_Question032.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    vector<int> scores(N);
+    for (int i = 0; i < N; ++i) {
+        cin >> scores[i];
+    }
+
+    int current_sum = 0;
+    int max_sum = scores[0]; 
+
+    for (int i = 0; i < N; ++i) {
+        current_sum = max(scores[i], current_sum + scores[i]);
+        max_sum = max(max_sum, current_sum);
+    }
+
+    cout << max_sum << endl;
+    return 0;
+}
