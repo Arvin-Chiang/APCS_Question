2e1aeb2247ad6c89af62cc7a1e5276273ba1bb62
Arvin-Chiang <arvinchiang673@gmail.com>
2025-07-30 14:29:48 +0800

    Add files via upload

---
+++ APCS_Question029/APCS_Question029/APCS_Question029.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    int x;
+    cin >> x;
+
+    int left = 0, right = n - 1;
+    int mid, count = 0;
+    bool found = false;
+
+    while (left <= right) {
+        count++;
+        mid = left + (right - left) / 2;
+
+        if (arr[mid] == x) {
+            cout << mid << " " << count << endl;
+            found = true;
+            break;
+        }
+        else if (arr[mid] < x) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+
+    if (!found) {
+        cout << "not found " << count << endl;
+    }
+
+    return 0;
+}
---
+++ APCS_Question030/APCS_Question030/APCS_Question030.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> bestFriend;
+vector<bool> visited;
+
+void dfs(int node) {
+    if (visited[node]) return;
+    visited[node] = true;
+    dfs(bestFriend[node]); 
+}
+
+int main() {
+    int N;
+    cin >> N;
+    bestFriend.resize(N);
+    visited.resize(N, false);
+
+    for (int i = 0; i < N; ++i) {
+        cin >> bestFriend[i];
+    }
+
+    int groupCount = 0;
+    for (int i = 0; i < N; ++i) {
+        if (!visited[i]) {
+            dfs(i);  
+            groupCount++;
+        }
+    }
+
+    cout << groupCount << endl;
+    return 0;
+}
