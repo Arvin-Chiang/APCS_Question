7cc14467fb90766c98aa74364cc8208f9cd9cbed
Arvin-Chiang <arvinchiang673@gmail.com>
2025-08-12 11:17:20 +0800

    Add files via upload

---
+++ APCS_Question042/APCS_Question042/APCS_Question042.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    int N = 1;
+    int a[75];
+    
+    int num = 0;
+    cin >> N;
+    for (int i = 0; i < 75; i++) {
+        a[i] = 0;
+    }
+    a[0] = a[0] = 1;
+    if(N > 1){
+        for (int b = 0; b < N-1; b++) {
+            a[0] = a[0] + 4 ^ b;
+        }
+    }
+
+    
+    cout << a[0] << endl;
+    return 0;
+}
